dffd9a4927d5c8b632cc9fcec8db51fc2bdd071a
Sonic Mine <70718556+SonicMine@users.noreply.github.com>
2020-09-04 12:10:16 +0300

    Add files via upload

---
+++ 1986VE9x_boch/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H 
+
+#include "mdr32fx.h"
+#include "core_cm3.h"
+#include "UART.h"
+#include "CPU_config.h"
+#include "types.h"
+#include "ports.h"
+#include "DAC.h"
+#include "timer.h"
+#include "function.h"
+#include "MPV.h"
+#include "PID.h"
+#include "time_scale.h"
+
+#endif
